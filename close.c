e23ac14b29b6dc19b7b911cca66bfae7271ea142
kjh-lk <1909834322@qq.com>
2020-05-11 23:20:37 -0700

    添加新文件close.c

---
+++ close.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/mman.h>
+
+#define BASE_addr 0x1E780000
+#define BASE_addr1 0x1E6E2000
+#define SIZE 0xff
+
+void poweroff_l(void)
+{
+	int fd;
+	fd=open("/dev/mem",O_RDWR|O_NDELAY);
+	unsigned int* map_base=(unsigned int*)mmap(NULL,SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,BASE_addr);
+	*(volatile unsigned int*)(map_base+0x24/4)|=(1<<9);//输出状态寄存器
+	*(volatile unsigned int*)(map_base+0x20/4)&=~(1<<9);//数据寄存器
+	usleep(1000000);
+	if(fd)
+		close(fd);
+	munmap(map_base,SIZE);
+}
+
+void ask_l(void)
+{
+	int fd;
+	fd=open("/dev/mem",O_RDWR|O_NDELAY);
+	unsigned int* map_base=(unsigned int*)mmap(NULL,SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,BASE_addr);
+	*(volatile unsigned int*)(map_base+0x24/4)|=(1<<11);//输出状态寄存器
+	*(volatile unsigned int*)(map_base+0x20/4)&=~(1<<11);//数据寄存器
+	usleep(1000000);
+	if(fd)
+		close(fd);
+	munmap(map_base,SIZE);
+}
+void scu70(void)
+{
+	int fd;
+	fd=open("/dev/mem",O_RDWR|O_NDELAY);
+	unsigned int* map_base=(unsigned int*)mmap(NULL,SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,BASE_addr1);
+	*(volatile unsigned int*)(map_base+0x70/4)|=(1<<19);
+	usleep(1000000);
+	if(fd)
+		close(fd);
+	munmap(map_base,SIZE);
+}
+
+int main(void)
+{
+	scu70();
+	poweroff_l();
+	ask_l();
+	return 0;
+}
+
